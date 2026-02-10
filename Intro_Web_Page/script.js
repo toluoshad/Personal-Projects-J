1f0d2e7370202d738b081f9b9e570f1f448bc9d6
ToluOsh <toluoshad@gmail.com>
2026-02-10 23:15:34 +0000

    Starting point for Portfolio page
    Restyled the whole index page to allow sections to divide the page and help reduce clutter, implemented a clear, concise summary of about me, and event listeners for scrolling along the page and lastly, changed styling to make it look cleaner

--- Intro_Web_Page/script.js
+++ Intro_Web_Page/script.js
@@ -1,1 +1,24 @@
-h
+// Highlight active nav link on scroll
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-menu a");
+
+window.addEventListener("scroll", () => {
+  let currentSection = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+
+    if (scrollY >= sectionTop - sectionHeight / 3) {
+      currentSection = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === `#${currentSection}`) {
+      link.classList.add("active");
+    }
+  });
+});
